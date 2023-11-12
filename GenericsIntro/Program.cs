632cffb470d047a7bd127066a1161879d646954c
Okan YILMAZ <okan_yilmaz28@hotmail.com>
2023-11-12 18:58:30 +0300

    Added Collections, Generics, OOP

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            list.Add("Engin");
+        }
+    }
+}
